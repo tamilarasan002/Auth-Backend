49c02215dcb9d4c358591115b0707565714c5ba1
AVA-Tamilarasan <127434215+AVA-Tamilarasan@users.noreply.github.com>
2023-07-22 13:09:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,65 +1,56 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const app = express();
-const port = 4000;
-const secretKey = 'my-private-key'; // Replace this with your actual secret key
+function App() {
+  const [task, setTask] = useState('');
+  const [tasks, setTasks] = useState([]);
 
-// Set up CORS with the frontend URL
-app.use(cors({ origin: process.env.MY_FRONTEND_URL }));
+  // Replace 'REACT_APP_BACKEND_URL' with your environment variable for the backend URL
+  const backendURL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:4000';
 
-app.use(bodyParser.json());
+  // Fetch tasks from the server when the component mounts
+  useEffect(() => {
+    fetchTasks();
+  }, []);
 
-// Middleware to check if the request is coming from the frontend
-function isFrontendRequest(req, res, next) {
-  const origin = req.headers.origin;
-  const frontendDomain = process.env.MY_FRONTEND_URL; // Use the environment variable directly
-
-  // Compare the request origin with the frontend domain
-  if (origin === frontendDomain) {
-    // If the request is coming from the frontend, bypass authentication and continue to the next middleware/route
-    return next();
-  }
-
-  // If the request is not coming from the frontend, proceed with authentication
-  authenticateToken(req, res, next);
-}
-
-// Authentication middleware
-function authenticateToken(req, res, next) {
-  const token = req.headers.authorization;
-
-  if (!token) {
-    return res.sendStatus(401);
-  }
-
-  jwt.verify(token, secretKey, { algorithms: ['HS256'] }, (err, decoded) => {
-    if (err) {
-      return res.sendStatus(403);
+  const fetchTasks = async () => {
+    try {
+      const response = await axios.get(`${backendURL}/api/tasks`);
+      setTasks(response.data);
+    } catch (error) {
+      console.error('Error fetching tasks:', error);
     }
+  };
 
-    req.developerPublicKey = decoded.sub;
-    next();
-  });
+  const addTask = async () => {
+    try {
+      await axios.post(`${backendURL}/api/tasks`, { task });
+      setTask('');
+      fetchTasks(); // Fetch updated tasks after adding a new one
+    } catch (error) {
+      console.error('Error adding task:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <div>
+        <input
+          type="text"
+          value={task}
+          onChange={(e) => setTask(e.target.value)}
+          placeholder="Enter task"
+        />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task.task}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-// In-memory tasks array
-let tasks = [];
-
-// Add a new task (protected route, bypassed for frontend requests)
-app.post('/api/tasks', isFrontendRequest, (req, res) => {
-  const { task } = req.body;
-  tasks.push({ task, developer: req.developerPublicKey });
-  res.status(201).json({ message: 'Task added successfully!' });
-});
-
-// Get all tasks (protected route, bypassed for frontend requests)
-app.get('/api/tasks', isFrontendRequest, (req, res) => {
-  res.json(tasks);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+export default App;

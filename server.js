6ff8b58ac96a9fc8fc48a9c94c4aca2032bddcca
AVA-Tamilarasan <Tamilarasan.D@avasoft.com>
2023-07-22 09:41:59 +0530

    final commit

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+const port = 4000;
+const secretKey = 'my-private-key'; // Replace this with your actual secret key
+
+
+app.use(cors({ origin: 'http://localhost:3000' }));
+app.use(cors());
+app.use(bodyParser.json());
+
+// Middleware to check if the request is coming from the frontend
+function isFrontendRequest(req, res, next) {
+  const origin = req.headers.origin;
+  const frontendDomain = 'http://localhost:3000'; // Replace 3000 with the port where your frontend is running
+
+  // Compare the request origin with the frontend domain
+  if (origin === frontendDomain) {
+    // If the request is coming from the frontend, bypass authentication and continue to the next middleware/route
+    return next();
+  }
+
+  // If the request is not coming from the frontend, proceed with authentication
+  authenticateToken(req, res, next);
+}
+
+// Authentication middleware
+function authenticateToken(req, res, next) {
+  const token = req.headers.authorization;
+
+  if (!token) {
+    return res.sendStatus(401);
+  }
+
+  jwt.verify(token, secretKey, { algorithms: ['HS256'] }, (err, decoded) => {
+    if (err) {
+      return res.sendStatus(403);
+    }
+
+    req.developerPublicKey = decoded.sub;
+    next();
+  });
+}
+
+// In-memory tasks array
+let tasks = [];
+
+// Add a new task (protected route, bypassed for frontend requests)
+app.post('/api/tasks', isFrontendRequest, (req, res) => {
+  const { task } = req.body;
+  tasks.push({ task, developer: req.developerPublicKey });
+  res.status(201).json({ message: 'Task added successfully!' });
+});
+
+// Get all tasks (protected route, bypassed for frontend requests)
+app.get('/api/tasks', isFrontendRequest, (req, res) => {
+  res.json(tasks);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+

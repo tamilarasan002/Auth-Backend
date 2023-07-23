7bd8051325df9ec3935c5955446dc227a575c01c
AVA-Tamilarasan <127434215+AVA-Tamilarasan@users.noreply.github.com>
2023-07-23 21:50:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
-// backend/index.js
-
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const axios = require('axios');
+const jwt = require('jsonwebtoken');
+const port = 4000;
 const app = express();
 
-app.use(cors());
+const secretKey = process.env.SECRET_KEY; // Replace this with your actual secret key
+
+// Set up CORS with the frontend URL
+app.use(cors({ origin: process.env.MY_FRONTEND_URL }));
+
 app.use(bodyParser.json());
 
-const authServerURL = 'http://localhost:5001';
+// Middleware to check if the request is coming from the frontend
+function isFrontendRequest(req, res, next) {
+  const origin = req.headers.origin;
+  const frontendDomain = process.env.MY_FRONTEND_URL; // Use the environment variable directly
 
+  // Compare the request origin with the frontend domain
+  if (origin === frontendDomain) {
+    // If the request is coming from the frontend, bypass authentication and continue to the next middleware/route
+    return next();
+  }
+
+  // If the request is not coming from the frontend, proceed with authentication
+  authenticateToken(req, res, next);
+}
+
+// Authentication middleware
 function authenticateToken(req, res, next) {
   const token = req.headers.authorization;
 
@@ -18,31 +35,45 @@ function authenticateToken(req, res, next) {
     return res.sendStatus(401);
   }
 
-  axios
-    .post(`${authServerURL}/verify-token`, { token })
-    .then((response) => {
-      if (response.data.valid) {
-        req.user = response.data.decoded.sub;
-        console.log('User authorized:', req.user);
-        next();
-      } else {
-        console.error('Token verification failed:', response.data.error);
-        res.sendStatus(403);
-      }
-    })
-    .catch((err) => {
-      console.error('Error verifying token:', err.message);
-      res.sendStatus(500);
-    });
+  jwt.verify(token, secretKey, { algorithms: ['HS256'] }, (err, decoded) => {
+    if (err) {
+      console.error('Token verification failed:', err);
+      return res.sendStatus(403);
+    }
+
+    // Extract the developer information (e.g., public key) from the token's payload
+    const developerPublicKey = decoded.sub;
+
+    // Associate the developer information with the request for future reference
+    req.developerPublicKey = developerPublicKey;
+
+    next();
+  });
 }
 
-app.get('/api/tasks', authenticateToken, (req, res) => {
-  // Replace this with your actual implementation to fetch tasks
-  const tasks = [{ id: 1, title: 'Task 1' }, { id: 2, title: 'Task 2' }];
+// In-memory tasks array
+let tasks = [];
+
+// Add a new task (protected route, bypassed for frontend requests)
+app.post('/api/tasks', isFrontendRequest, (req, res) => {
+  const { task } = req.body;
+
+  // Use the developer public key associated with the request
+  const developerPublicKey = req.developerPublicKey;
+
+  tasks.push({ task, developer: developerPublicKey });
+  res.status(201).json({ message: 'Task added successfully!' });
+});
+
+// Get all tasks (protected route, bypassed for frontend requests)
+app.get('/api/tasks', isFrontendRequest, (req, res) => {
   res.json(tasks);
 });
 
-const port = 4000;
-app.listen(port, () => {
-  console.log(`Backend service is running on http://localhost:${port}`);
+// Update the listening address to 0.0.0.0 to make the server accessible to other services within the cluster
+const host = '0.0.0.0';
+
+app.listen(port, host, () => {
+  console.log(`Backend service is running on http://${host}:${port}`);
 });
+
